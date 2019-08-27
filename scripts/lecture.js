682ae427baa859009d91aa80972f6deb6cd8e04c
Wei-Hao Wang <wwang03@risd.edu>
2019-08-27 15:03:54 -0400

    w
    w

--- scripts/lecture.js
+++ scripts/lecture.js
@@ -2,7 +2,17 @@ var data_lecture = [
 {
 	'infoOne':'1',
 	'infoTwo':'8/27',
-	'title':'Creative & </br>Computing',
-	'hyperLink':'TBA'
+	'title':'Intro',
+	'hyperLink':'media/resource/lectures/w1-1_intro.pdf'
+},{
+	'infoOne':'1',
+	'infoTwo':'8/27',
+	'title':'Basic Computing',
+	'hyperLink':'media/resource/lectures/w1-2_basicComputing.pdf'
+},{
+	'infoOne':'1',
+	'infoTwo':'8/27',
+	'title':'HTML 0',
+	'hyperLink':'media/resource/lectures/w1-3_html0.pdf'
 }
 ];
