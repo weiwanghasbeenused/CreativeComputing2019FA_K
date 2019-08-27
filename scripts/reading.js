b5b0b205d5d0bc6d51d97ec7d09a455dc8107a86
Wei-Hao Wang <wwang03@risd.edu>
2019-08-27 17:46:03 -0400

    w
    w

--- scripts/reading.js
+++ scripts/reading.js
@@ -21,10 +21,10 @@ var data_reading = [
 	'title':'Clive Thompson on the Future of Reading in a Digital World',
 	'hyperLink':'https://www.wired.com/2009/05/st-thompson-10/'
 },{
-	'infoOne':'Cecilia D&rsquo;Anastasio',
-	'infoTwo':'2015',
-	'title':'Why Video Games Can&rsquo;t Teach You Empathy (follow-up of dys4ia Game)',
-	'hyperLink':'https://www.vice.com/en_us/article/mgbwpv/empathy-games-dont-exist'
+	'infoOne':'Marlies Peeters',
+	'infoTwo':'2016',
+	'title':'Designing in Liquid Times: Generative Graphic Design in an Age of Uncertainty',
+	'hyperLink':'http://adht.parsons.edu/designstudies/plot/designing-in-liquid-times-generative-graphic-design-in-an-age-of-uncertainty/'
 },{
 	'infoOne':'Bret Victor',
 	'infoTwo':'2012',
@@ -36,10 +36,10 @@ var data_reading = [
 	'title':'dys4ia Game',
 	'hyperLink':'https://freegames.org/dys4ia/'
 },{
-	'infoOne':'Marlies Peeters',
-	'infoTwo':'2016',
-	'title':'Designing in Liquid Times: Generative Graphic Design in an Age of Uncertainty',
-	'hyperLink':'http://adht.parsons.edu/designstudies/plot/designing-in-liquid-times-generative-graphic-design-in-an-age-of-uncertainty/'
+	'infoOne':'Cecilia D&rsquo;Anastasio',
+	'infoTwo':'2015',
+	'title':'Why Video Games Can&rsquo;t Teach You Empathy (follow-up of dys4ia Game)',
+	'hyperLink':'https://www.vice.com/en_us/article/mgbwpv/empathy-games-dont-exist'
 }
 ];
 
