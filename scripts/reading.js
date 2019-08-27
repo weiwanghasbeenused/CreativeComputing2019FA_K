d1072561535546117914843c03b6f0f9a08109f2
Wei-Hao Wang <wwang03@risd.edu>
2019-08-27 15:39:56 -0400

    w
    w

--- scripts/reading.js
+++ scripts/reading.js
@@ -13,8 +13,18 @@ var data_reading = [
 	
 	'infoOne':'Guy Debord',
 	'infoTwo':'1958',
-	'title':'Theory of D&#x000E9;rive',
+	'title':'Theory of the D&#x000E9;rive',
 	'hyperLink':'media/resource/reading/TheoryOfDerive.pdf'
+},{
+	'infoOne':'Clive Thompson',
+	'infoTwo':'2009',
+	'title':'Clive Thompson on the Future of Reading in a Digital World',
+	'hyperLink':'https://www.wired.com/2009/05/st-thompson-10/'
+},{
+	'infoOne':'Cecilia D&rsquo;Anastasio',
+	'infoTwo':'2015',
+	'title':'Why Video Games Can&rsquo;t Teach You Empathy (follow-up of dys4ia Game)',
+	'hyperLink':'https://www.vice.com/en_us/article/mgbwpv/empathy-games-dont-exist'
 },{
 	'infoOne':'Bret Victor',
 	'infoTwo':'2012',
@@ -26,20 +36,10 @@ var data_reading = [
 	'title':'dys4ia Game',
 	'hyperLink':'https://freegames.org/dys4ia/'
 },{
-	'infoOne':'Cecilia D&rsquo;Anastasio',
-	'infoTwo':'2015',
-	'title':'Why Video Games Can&rsquo;t Teach You Empathy (follow-up of dys4ia Game)',
-	'hyperLink':'https://www.vice.com/en_us/article/mgbwpv/empathy-games-dont-exist'
-},{
 	'infoOne':'Marlies Peeters',
 	'infoTwo':'2016',
 	'title':'Designing in Liquid Times: Generative Graphic Design in an Age of Uncertainty',
 	'hyperLink':'http://adht.parsons.edu/designstudies/plot/designing-in-liquid-times-generative-graphic-design-in-an-age-of-uncertainty/'
-},{
-	'infoOne':'Clive Thompson',
-	'infoTwo':'2009',
-	'title':'Clive Thompson on the Future of Reading in a Digital World',
-	'hyperLink':'https://www.wired.com/2009/05/st-thompson-10/'
 }
 ];
 
