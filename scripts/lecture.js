729982d9c337d4f23e2e6885e639444e376fb4dd
Wei-Hao Wang <wwang03@risd.edu>
2019-09-02 22:38:45 -0400

    q
    q

--- scripts/lecture.js
+++ scripts/lecture.js
@@ -14,5 +14,11 @@ var data_lecture = [
 	'infoTwo':'8/27',
 	'title':'HTML 0',
 	'hyperLink':'media/resource/lectures/w1-3_html0.pdf'
+},{
+	'infoOne':'2',
+	'infoTwo':'9/3',
+	'title':'HTML 1',
+	'hyperLink':'https://docs.google.com/presentation/d/1zOPDLJfPm3rYdW6R77D40kJQSL39IRM8WExZdEyATtU/edit?usp=sharing'
 }
+
 ];
